28e613d1169fa4a9a9f8749c2f0d61784cfcf15f
UekusaTomoki <tmkueks19@gmail.com>
2015-12-11 15:30:12 +0900

    recording some methods

--- src/cabo8.py
+++ src/cabo8.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import CaboCha
+# @author 鈴木
+# 立地に関連する辞書を最初に表示
+# 次に係り受け解析のペアを表示 形式：[係元],[係先],[信頼度]
+# 最後に立地辞書と単語のマッチを調べる出力
+
+from os import path
+import CaboCha # debug時はコメントアウト
 from xml.etree.ElementTree import *
 
+# このプログラムの絶対パスを取得
+APP_ROOT = path.dirname( path.abspath( __file__ ) )
+
 def cabo(sentence):
- c = CaboCha.Parser("")
- tree =  c.parse(sentence)
+    c = CaboCha.Parser("")
+    tree =  c.parse(sentence)
 
- xmlString = tree.toString(CaboCha.FORMAT_XML)
- return xmlString
- elem = fromstring(xmlString)
- 
+    xmlString = tree.toString(CaboCha.FORMAT_XML)
+    return xmlString
+    elem = fromstring(xmlString)
 
+# 単語を取得する
 def get_word(tree, chunk):
     surface = ''
     for i in range(chunk.token_pos, chunk.token_pos + chunk.token_size):
@@ -28,6 +37,7 @@ def get_word(tree, chunk):
             break
     return surface
 
+# 単語を取得する
 def get_2_words(line):
     cp = CaboCha.Parser('-f1')
     tree = cp.parse(line)
@@ -48,56 +58,67 @@ def get_2_words(line):
             tuples.append((from_surface, to_surface))
     return tuples
 
+# main (ここからスタート)
 def start(check_word):
- f1 = open('2.txt')
- locate_dic = f1.read()  
- #f.close()
- location_dic = []
- location_dic = locate_dic.split(',') 
- print('立地辞書:'+str(location_dic))
- print("----------------------------------------")
-
- pair = []
- for i in range(1,3):
-   f2 = open('test%s.txt'%(i),'r')
-   hoteldata1 = f2.readline()
-   while hoteldata1:
-     xmlString = cabo(hoteldata1)
-     elem = fromstring(xmlString)
-
-     values = [] #係り値1.3
-     for chunk in elem.findall(".//chunk"):
-       values.append(chunk.get("score"))   
-     
-     kakariset = [] #料理-->美味しい
-     tuples = get_2_words(hoteldata1)
-     for t in tuples:
-       kakariset.append([t[0],t[1]]) #pair = []#kakarisetとvaluesを一組
-    
-     for i in range(len(kakariset)) :
-       x=(float(values[i]))
-       if ( x > 0.0):
-         pair.append([kakariset[i][0],kakariset[i][1],values[i]])
-     
-     hoteldata1 = f2.readline() 
- for j in pair :
-   print (j[0]+","+j[1]+","+j[2])
- f1.close
- f2.close
- print("----------------------------------------")
+    print APP_ROOT
+    # ファイルを取得
+    f1 = open(APP_ROOT+'/../data/location_dictionary.txt')
+    locate_dic = f1.read()
+    #f.close()
+    # 立地辞書の生成
+    location_dic = []
+    location_dic = locate_dic.split(',')
+    print('立地辞書:')
+    print location_dic
+    print("----------------------------------------")
 
 
- limitdata=[]
- for i in pair:
-   kakarimoto = (",".join(i)).split(",")[0]
-   print(str(kakarimoto))
-   print(str(location_dic[0]))
-   if(str(kakarimoto)==float(location_dic[i])):
-     print("[match]")
-   print("------")
+    pair = []
+    reliability = 0.0;# どれくらい信頼性を担保するのか設定する
+    # 係先ペアと点数の保存
+    for i in range(1,3):
+        # testデータを読み込み
+        f2 = open(APP_ROOT+'/../data/test%s.txt'%(i),'r')
+        hoteldata1 = f2.readline()
+        while hoteldata1:
+            xmlString = cabo(hoteldata1)
+            elem = fromstring(xmlString)
+
+            values = [] #係り値1.3
+        for chunk in elem.findall(".//chunk"):
+            values.append(chunk.get("score"))
+
+            kakariset = [] #料理-->美味しい
+            tuples = get_2_words(hoteldata1)
+            for t in tuples:
+                kakariset.append([t[0],t[1]]) #pair = []#kakarisetとvaluesを一組
+
+            for i in range(len(kakariset)) :
+                # 係値をxに保存
+                x=(float(values[i]))
+                if ( x > reliability):
+                    pair.append([kakariset[i][0],kakariset[i][1],values[i]])
+
+            hoteldata1 = f2.readline()
+    # 係先のデータを表示
+    # 信頼度が＋のものだけを保存している
+    for j in pair :
+       print (j[0]+","+j[1]+","+j[2])
+    f1.close
+    f2.close
+    print("----------------------------------------")
 
 
+    limitdata=[]
+    for i in pair:
+        kakarimoto = (",".join(i)).split(",")[0]
+        print(str(kakarimoto))
+        print(str(location_dic[0]))
+        if(str(kakarimoto)==location_dic[i]):
+            print("[match]")
+    print("------")
 
-   #if(kakarimoto == str(location_dic[i])):
-    # limit += pair[i]
-     #print(limit)
+# コマンドラインからこのプログラムを読んだときだけこれを実行する
+if __name__ == '__main__':
+    # startメソッドの軌道
+    start("")
